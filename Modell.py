9653e98b8a60044135dc7f8ce0c33556e2feb21a
I550808 <robin.quendt@sap.com>
2023-11-12 13:37:45 +0100

    First Wordlist

---
+++ Modell.py
@@ -0,0 +1,55 @@
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from textblob_de import TextBlobDE
+
+
+
+
+input_file_name = "article_text.txt"
+
+input_file_name2 = "second_text.txt"
+
+
+
+
+# Initialisiere den String für den gesamten Text
+full_text = ""
+full_text2 = ""
+
+# Öffne die Textdatei und lies den Inhalt Zeile für Zeile
+with open(input_file_name, 'r') as input_file:
+    for line in input_file:
+        full_text += line  # Füge die Zeile zum gesamten Text hinzu
+
+with open(input_file_name2, 'r') as input_file:
+    for line in input_file:
+        full_text2 += line  # Füge die Zeile zum gesamten Text hinzu
+    
+
+
+word_list = full_text.split()
+word_list2 = full_text2.split()
+
+word_list = set(word_list)
+word_list2 = set(word_list2)
+
+
+def find_diffrent_words(word_list,word_list2):
+    liste = word_list.difference(word_list2)
+    liste = list(liste)
+    return liste
+   
+
+print(find_diffrent_words(word_list,word_list2))
+
+
+
+
+
+
+
+
+
+
+
+
--- find_adjectives.py
+++ find_adjectives.py
@@ -2,6 +2,14 @@ from nltk import pos_tag
 from nltk.tokenize import word_tokenize
 from nltk.tokenize import RegexpTokenizer
 from nltk.probability import FreqDist
+import nltk
+from nltk import word_tokenize, pos_tag
+
+text = ""
+input_file_name = "article_text.txt"
+with open(input_file_name, 'r') as input_file:
+    for line in input_file:
+        text += line  # Füge die Zeile zum gesamten Text hinzu
 
 def get_number_of_words(text):
     
@@ -16,7 +24,7 @@ def get_number_of_adjectives(text):
     words = word_tokenize(text)
     tagged_words = pos_tag(words)
     
-    adjectives = [word for word, tag in tagged_words if tag in ['NN', 'NNR', 'NNS']]
+    adjectives = [word for word, tag in tagged_words if tag in ["JJ"]]
     
     return(len(adjectives))
 
@@ -26,7 +34,28 @@ def get_most_common_words(text):
     return(fdist.most_common(10))
 
 
-text = "Das is mir egal und dir? He was soll das denn?"
+
 print("Adjective Ratio:", get_number_of_adjectives(text) / get_number_of_words(text))
 print(get_most_common_words(text))
+words = word_tokenize(text)
+tagged_words = pos_tag(words)
+adjectives = [word for word, tag in tagged_words if tag in ['JJ']]
+print(adjectives)
 
+print("neu")
+
+nltk.download('punkt')  # Nur beim ersten Aufruf, um den Tokenizer herunterzuladen
+
+
+
+# Tokenisieren des Texts in Wörter
+words = word_tokenize(text)
+
+# POS-Tagging der Wörter
+pos_tags = pos_tag(words)
+
+# Extrahieren der Adjektive (Wörter mit dem "JJ"-Tag)
+adjectives = [word for word, pos in pos_tags if pos == "JJ"]
+
+# Ausgabe der Adjektive
+print("Adjektive:", adjectives)
